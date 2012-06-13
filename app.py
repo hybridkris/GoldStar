f5ebc05484f32dc2cfb77f967b673c4da601bf1f
Jay Ostinowsky <dukebdfan@comcast.net>
2012-06-13 09:04:15 -0400

    added www.

--- app.py
+++ app.py
@@ -269,12 +269,12 @@ def tweet(star_id):
 			status = '#GoldStar: @' + star.owner.twitterUser + ' for ' + star.description + ' #' + star.hashtag + ' www.Goldstars.me'
 		else:
 			fullName = star.owner.firstName + ' ' + star.owner.lastName
-			status = '#GoldStar: ' + fullName + ' for ' + star.description + ' #' + star.hashtag + ' Goldstars.me'
+			status = '#GoldStar: ' + fullName + ' for ' + star.description + ' #' + star.hashtag + ' www.Goldstars.me'
 		if len(status) > 140:
 			cutDescriptionBy = len(status) - 140
 			cutDescriptionBy = len(star.description) - cutDescriptionBy
 			if star.owner.twitterUser:
-				status = '#GoldStar: @' + star.owner.twitterUser + ' for ' + star.description[0:cutDescriptionBy] + ' #' + star.hashtag + ' Goldstars.me'
+				status = '#GoldStar: @' + star.owner.twitterUser + ' for ' + star.description[0:cutDescriptionBy] + ' #' + star.hashtag + ' www.Goldstars.me'
 			else:
 				status = '#GoldStar: ' + fullName + ' for ' + star.description[0:cutDescriptionBy] + ' #' + star.hashtag + ' www.Goldstars.me'
 		resp = twitter.post('statuses/update.json', data = {'status': status})
